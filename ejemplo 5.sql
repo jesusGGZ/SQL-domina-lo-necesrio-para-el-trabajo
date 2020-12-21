32f4dd633a0258285328c19debbf19ff2f41654c
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-21 15:33:39 -0600

    uso de where

---
+++ ejemplo 5.sql
@@ -0,0 +1,11 @@
+select Mnomateria, Mnombre, Mcred, Mnocarrera, Mcostolab
+from Materia
+where Mcostolab = 100;
+
+select Mnomateria, Mnombre, Mcred, Mnocarrera, Mcostolab
+from Materia
+where Mcostolab < 100;
+
+select Mnomateria, Mnombre, Mcred, Mnocarrera, Mcostolab
+from Materia
+where Mcostolab >= 100;
