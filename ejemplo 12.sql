add27e17678de890f9a498d3e9c97c5c8b1cee10
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-22 08:46:23 -0600

    Alias de tablas

---
+++ ejemplo 12.sql
@@ -0,0 +1,13 @@
+SELECT carrera.Cnocarrera, carrera.Cnombre
+FROM carrera;
+
+SELECT carrera.Cnocarrera, carrera.Cnombre
+FROM carrera 
+WHERE carrera.Cnocarrera ='32';
+
+SELECT * FROM Materia;
+
+SELECT M.Mnomateria, M.Mnombre 
+FROM Materia M
+WHERE M.Mnomateria = '0076';
+
